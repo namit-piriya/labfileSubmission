2e0227fca0f8fff44c3452d8942c389cb38c9cc2
namit-piriya <namitpiriya945@gmail.com>
2020-10-07 20:35:50 +0530

    formatting changes and some minor changes

--- controllers/student_c.js
+++ controllers/student_c.js
@@ -1,95 +1,94 @@
 const StudentModel = require("../models/student_m");
-const {
-	isEmpty
-} = require("lodash");
-const lognsend = require("../util/log").log;
+const { isEmpty } = require("lodash");
+const log = require("../util/log").log;
 const checkEmail = require("../util/gen_util").checkEmail;
 const checkPass = require("../util/gen_util").checkPass;
 
 exports.registerStudent = async (req, res) => {
-	let isError;
-	let devErr;
-	const {
-		body
-	} = req;
-	const {
-		email,
-		name,
-		password,
-		enrollno,
-		dept
-	} = body;
-	let resultObj;
+  let isError, response;
+  let devErr;
+  const { body } = req;
+  const { email, fullName, password, enrollno, dept } = body;
 
-	debugCon({
-		email,
-		name,
-		password,
-		enrollno,
-		dept
-	});
+  console.log(email, fullName, password, enrollno, dept);
+  debugCon({
+    email,
+    fullName,
+    password,
+    enrollno,
+    dept,
+  });
 
-	if (isEmpty(checkEmail(email))) {
-		resultObj = {
-			userErr: "please Provide valid  email",
-			code: 400,
-		};
-		return await lognsend(resultObj);
-	}
+  if (!email || !password | !enrollno || !dept || !fullName) {
+    response = {
+      userErr: "please Provide valid input or email",
+      code: 400,
+    };
+  } else if (isEmpty(checkEmail(email))) {
+    response = {
+      userErr: "please Provide valid email",
+      code: 400,
+    };
+  } else if (isEmpty(checkPass(password))) {
+    response = {
+      userErr: "please Provide valid password",
+      code: 400,
+    };
+  }
 
-	if (isEmpty(checkPass(password))) {
-		resultObj = {
-			userErr: "please Provide valid password",
-			code: 400,
-		};
-		return await lognsend(resultObj);
-	}
-
-	if (!email || !password | !enrollno || !dept || !name) {
-		resultObj = {
-			userErr: "please Provide valid input or email",
-			code: 400,
-		};
-		return await lognsend(resultObj);
-	}
-	const student = new StudentModel(name, email, enrollno, password, dept);
-	let result, obj;
-	try {
-		result = await student.registerStudent();
-		if (result === true) {
-			obj = {
-				code: 200,
-				error: false,
-				msg: "registration successful, procceed with verification.",
-			};
-		} else if (result == false) {
-			obj = {
-				code: 200,
-				error: false,
-				msg: "please contact to hod about your sem and year"
-			}
-		} else if (result === "EMAIL_EXISTS") {
-			obj = {
-				code: 400,
-				userErr: "Email already exists.",
-			};
-		} else {
-			debugCon("result is ", result);
-		}
-	} catch (error) {
-		isError = true;
-		devErr = error;
-		obj = {
-			code: 500,
-			error: true,
-			msg: "please contact to admin"
-		}
-	} finally {
-		res.status(obj.code).json(obj);
-		await lognsend({
-			responseObj: obj,
-			isError,
-			devErr
-		});
-	}
+  if (response !== undefined && response.userErr) {
+    res.status(response.code).json(response);
+    return await log({
+      response,
+      isError,
+      devErr,
+    });
+  }
+  const student = new StudentModel(fullName, email, enrollno, password, dept);
+  let result, obj;
+  try {
+    result = await student.registerStudent();
+    if (result === true) {
+      response = {
+        code: 200,
+        error: false,
+        msg: "registration successful, procceed with verification.",
+      };
+    } else if (result == false) {
+      response = {
+        code: 200,
+        error: false,
+        msg: "please contact to hod about your sem and year",
+      };
+    } else if (result === "EMAIL_EXISTS") {
+      response = {
+        code: 400,
+        userErr: "Email already exists.",
+      };
+    } else {
+      response = {
+        msg: errMessage,
+        code: 500,
+        error: true,
+      };
+      isError = true;
+      devErr = error;
+      debugCon("result is ", result);
+    }
+  } catch (error) {
+    isError = true;
+    devErr = error;
+    response = {
+      code: 500,
+      error: true,
+      msg: "please contact to admin",
+    };
+  } finally {
+    res.status(response.code).json(response);
+    await log({
+      response,
+      isError,
+      devErr,
+    });
+  }
 };
--- models/student_m.js
+++ models/student_m.js
@@ -1,85 +1,83 @@
 // importing db connection
 const User = require("./user_m");
 const dbConnection = require("../util/db_util");
-const getSemAndYear = require('../util/gen_util').getSemAndYear;
-const hashUtil = require('../util/hash_util');
-const _ = require('lodash');
+const getSemAndYear = require("../util/gen_util").getSemAndYear;
+const hashUtil = require("../util/hash_util");
+const _ = require("lodash");
 
 // this will hold the connection
 let db;
 module.exports = class Student {
-	constructor(name, email, enrollno, password, dept) {
-		this.name = name;
-		this.email = email;
-		this.dept = dept;
-		this.enrollno = enrollno;
-		this.password = password;
-		this.verified = false;
-	}
-	// inserting data into db of user
-	async registerStudent() {
-		try {
-			let result;
-			db = !db ? await dbConnection() : db;
-			result = await db
-				.collection("students")
-				.findOne({
-					$or: [{
-						_id: this.email
-					}, {
-						enrollno: this.enrollno
-					}]
-				});
+  constructor(name, email, enrollno, password, dept) {
+    this.name = name;
+    this.email = email;
+    this.dept = dept;
+    this.enrollno = enrollno;
+    this.password = password;
+    this.verified = false;
+  }
+  // inserting data into db of user
+  async registerStudent() {
+    try {
+      let result;
+      db = !db ? await dbConnection() : db;
+      result = await db.collection("students").findOne({
+        $or: [
+          {
+            _id: this.email,
+          },
+          {
+            enrollno: this.enrollno,
+          },
+        ],
+      });
 
-			if (result !== null) return "EMAIL_EXISTS";
-			else {
-				const {
-					sem,
-					year
-				} = getSemAndYear(this.enrollno);
-				debugCon(sem, year);
-				console.log(sem, year);
-				if (!getSemAndYear(this.enrollno))
-					return false;
-				let hashedPass = await hashUtil.hashPass(this.password);
-				debugCon("after hashpass");
-				result = await db.collection("students").insertOne({
-					tokens: [],
-					name: this.name,
-					_id: this.email,
-					dept: this.dept,
-					enrollno: this.enrollno,
-					semester: sem,
-					year: year,
-					password: hashedPass,
-					verified: false,
-				});
-				debugCon("after insertion");
-				return true;
-			}
-		} catch (error) {
-			// console.log(error)
-			throw new Error(error);
-		}
-	}
+      if (result !== null) return "EMAIL_EXISTS";
+      else {
+        const { sem, year } = getSemAndYear(this.enrollno);
+        debugCon(sem, year);
+        console.log(sem, year);
+        if (!getSemAndYear(this.enrollno)) return false;
+        let hashedPass = await hashUtil.hashPass(this.password);
+        debugCon("after hashpass");
+        result = await db.collection("students").insertOne({
+          tokens: [],
+          name: this.name,
+          _id: this.email,
+          dept: this.dept,
+          enrollno: this.enrollno,
+          semester: sem,
+          year: year,
+          password: hashedPass,
+          verified: false,
+        });
+        console.log(result);
+        debugCon("after insertion");
+        return true;
+      }
+    } catch (error) {
+      // console.log(error)
+      throw new Error(error);
+    }
+  }
 
-	static async loginStudent(email, password) {
-		try {
-			let result;
-			result = await User.loginUser(email, password, "students");
-			return result;
-		} catch (error) {
-			throw error;
-		}
-	}
+  static async loginStudent(email, password) {
+    try {
+      let result;
+      result = await User.loginUser(email, password, "students");
+      return result;
+    } catch (error) {
+      throw error;
+    }
+  }
 
-	static async verifyStudent(email, verifiedBy) {
-		try {
-			let result;
-			result = await User.verifyUser(email, verifiedBy, "students");
-			return result;
-		} catch (error) {
-			throw new Error(error);
-		}
-	}
+  static async verifyStudent(email, verifiedBy) {
+    try {
+      let result;
+      result = await User.verifyUser(email, verifiedBy, "students");
+      return result;
+    } catch (error) {
+      throw new Error(error);
+    }
+  }
 };
