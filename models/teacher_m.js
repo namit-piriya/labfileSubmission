71747b11ce0ac8dd8816ae451aa9db575fc57a9f
namit-piriya <namitpiriya945@gmail.com>
2020-10-07 20:39:13 +0530

    minor change in addSubject: added subName and sem to be stored in the db

--- models/teacher_m.js
+++ models/teacher_m.js
@@ -1,35 +1,36 @@
 let getDbConnection = require("../util/db_util");
 const User = require("./user_m");
-const hashUtil = require('../util/hash_util');
+const hashUtil = require("../util/hash_util");
+const { getSemFromSubcode } = require("../util/gen_util");
 let db;
 class Teacher {
-	static async registerTeacher(email, name, password, dept) {
-		try {
-			let result;
-			db = !db ? await getDbConnection() : db;
-			result = await db.collection("teachers").findOne({
-				_id: email
-			});
-			if (result) return "EMAIL_EXISTS";
-			// check if a student is trying to register with same email
+  static async registerTeacher(email, name, password, dept) {
+    try {
+      let result;
+      db = !db ? await getDbConnection() : db;
+      result = await db.collection("teachers").findOne({
+        _id: email,
+      });
+      if (result) return "EMAIL_EXISTS";
+      // check if a student is trying to register with same email
 
-			result = await db.collection("students").findOne({
-				_id: email
-			});
+      result = await db.collection("students").findOne({
+        _id: email,
+      });
 
-			if (result) return "STUDENT_EMAIL_EXISTS";
-			else {
-				let hashedPass = await hashUtil.hashPass(password);
-				result = await db.collection("teachers").insertOne({
-					name: name,
-					_id: email,
-					dept,
-					password: hashedPass,
-					verified: false,
-					joinedOn: Date.now(),
-					teaches: [],
-				});
-				/*
+      if (result) return "STUDENT_EMAIL_EXISTS";
+      else {
+        let hashedPass = await hashUtil.hashPass(password);
+        result = await db.collection("teachers").insertOne({
+          name: name,
+          _id: email,
+          dept,
+          password: hashedPass,
+          verified: false,
+          joinedOn: Date.now(),
+          teaches: [],
+        });
+        /*
 				teaches ;[
 				  {
 				    subName:"",
@@ -38,43 +39,48 @@ class Teacher {
 				  }
 				]
 				 */
-				if (result) {
-					return true;
-				}
-			}
-		} catch (error) {
-			throw new Error(error);
-		}
-	}
+        if (result) {
+          return true;
+        }
+      }
+    } catch (error) {
+      throw new Error(error);
+    }
+  }
 
-	static async loginTeacher(email, password) {
-		try {
-			let result;
-			result = await User.loginUser(email, password, "teachers");
-			return result;
-		} catch (error) {
-			throw new Error(error);
-		}
-	}
+  static async loginTeacher(email, password) {
+    try {
+      let result;
+      result = await User.loginUser(email, password, "teachers");
+      return result;
+    } catch (error) {
+      throw new Error(error);
+    }
+  }
 
-	static async addSubject(subcode, teacherEmail) {
-		try {
-			db = !db ? await getDbConnection() : db;
-			const result = await db.collection("teachers").findOneAndUpdate({
-				_id: teacherEmail
-			}, {
-				$push: {
-					teaches: subcode
-				}
-			});
-			// console.log(result);
-			if (result.lastErrorObject.n === 1) {
-				return true
-			} else false;
-		} catch (error) {
-			throw new Error(error);
-		}
-	}
+  static async addSubject(subName, subcode, teacherEmail) {
+    try {
+      db = !db ? await getDbConnection() : db;
+      let sem = getSemFromSubcode(subcode);
+      const result = await db.collection("teachers").findOneAndUpdate(
+        {
+          _id: teacherEmail,
+        },
+        {
+          $push: {
+            teaches: { subcode, subName, sem },
+          },
+        }
+      );
+      console.log(result);
+      // console.log(result);
+      if (result.lastErrorObject.n === 1) {
+        return true;
+      } else false;
+    } catch (error) {
+      throw new Error(error);
+    }
+  }
 }
 
 module.exports = Teacher;
