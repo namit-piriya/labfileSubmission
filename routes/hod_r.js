bfaa1c0eef2b5d8060e2953bece05ec808ddd53f
namit-piriya <namitpiriya945@gmail.com>
2020-10-07 20:44:55 +0530

    discriptive routes added comments, added info_of_teachers route

--- routes/hod_r.js
+++ routes/hod_r.js
@@ -1,24 +1,31 @@
-const hodController = require('../controllers/hod_c');
-const router = require('express').Router();
-const isAuthorized = require('../middleware/middleware').isAuthorized;
+const hodController = require("../controllers/hod_c");
+const router = require("express").Router();
+const isAuthorized = require("../middleware/middleware").isAuthorized;
 
 router.post("/save_curriculum", isAuthorized, hodController.saveCurriculum);
 
 router.post("/update_curriculum", isAuthorized, hodController.updateCurriculum);
 
-router.get("/get_unverified_users", isAuthorized, hodController.getUnverifiedUsers);
+router.get(
+  "/get_unverified_users",
+  isAuthorized,
+  hodController.getUnverifiedUsers
+);
 
-router.get("/verify_user/:email", isAuthorized, hodController.verifyUser);
+// pass user email in the body as email: to verify that user and user_type as query parameter
+router.post("/verify_user", isAuthorized, hodController.verifyUser);
 
+// subName , subcode , and teacherEmail  in the body
 router.post("/add_sub_to_teacher", isAuthorized, hodController.addSubToTeacher);
 
-// router.get("/info_of_sem/:sem",isAuthorized,hodController.infoOfSem);
+// router.get("/info_of_sem/:sem", isAuthorized, hodController.infoOfSem);
+
+router.get("/info_of_teachers", isAuthorized, hodController.infoOfTeachers);
 
 // router.get("/get_subject_in_sem", isAuthorized, hodController.getSubInSem);
 
 // router.get("/get_unassigned_subjects", isAuthorized, hodController.getUnassignedSubjects);
 
 // router.get("generate_marksheet_of_subject", isAuthorized, hodController.generateMarksheet);
-
 
 module.exports = router;
