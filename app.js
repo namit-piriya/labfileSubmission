b91aaecdb52b0854255280a64183958a0e8b9d3a
namit-piriya <namitpiriya945@gmail.com>
2020-10-08 13:45:20 +0530

    minor changes to support cors

--- app.js
+++ app.js
@@ -1,56 +1,88 @@
 const dotenv = require("dotenv");
 dotenv.config();
+// const helmet = require("helmet");
+const morgan = require("morgan");
+const cors = require("cors");
 const express = require("express");
+
+const log = require("./util/log").log;
 const check_init = require("./util/check_init");
+const appRouter = require("./routes/app_r");
 const authRouter = require("./routes/auth_r");
 const studentRouter = require("./routes/student_r");
 const teacherRouter = require("./routes/teacher_r");
 const hodRouter = require("./routes/hod_r");
 
 global.whereis = process.env.NODE_ENV;
-global.errMessage = "something wrong happened please contact admin"
+global.errMessage = "something wrong happened please contact admin";
 global.debugCon = function (...obj) {
-	if ((whereis !== "prod")) {
-		console.log("on date " + new Date().toString() + " object is " + JSON.stringify(obj));
-	}
+  if (whereis !== "prod") {
+    console.log(
+      "on date " + new Date().toString() + " object is " + JSON.stringify(obj)
+    );
+  }
 };
 
 // app.use(configured);
 const app = express();
+
 // for post requests, parsing body
 const bodyParser = require("body-parser");
+const { response } = require("express");
+const { isError } = require("lodash");
 
 // This will help parsing the body
-app.use(bodyParser.urlencoded({
-	extended: true
-}));
+app.use(
+  bodyParser.urlencoded({
+    extended: true,
+  })
+);
 
 // for parsing json data
-app.use(bodyParser.json())
+app.use(bodyParser.json());
 app.use(express.json());
 
+app.use(async (err, req, res, next) => {
+  console.log("my error handler");
+  if (err) {
+    console.log(err);
+    let isError = true;
+    let devErr = err;
+    let response = { msg: errMessage, code: 500 };
+    res.status(response.code).json();
+    return await log({ response, isError, devErr });
+  }
+  next();
+});
+
+// logging with morgan
+app.use(morgan("combined"));
+// using cors
+app.use(cors());
+
+// using routers
+app.use("/app", appRouter);
 app.use("/auth", authRouter);
 app.use("/student", studentRouter);
 app.use("/hod", hodRouter);
 app.use("/teacher", teacherRouter);
 
 app.all("*", (req, res) => {
-	return res.status(404).json({
-		msg: "route not available"
-	});
+  return res.status(404).json({
+    msg: "route not available",
+  });
 });
-
 
 // check db and logs util
 (async () => {
-	await check_init();
+  await check_init();
 })()
-.then((result) => {
-		const port = process.env.PORT || 8000;
-		app.listen(port);
-		console.log(`listening on ${port}....`);
-	})
-	.catch((err) => {
-		console.log(err);
-		process.exit(1);
-	});
+  .then((result) => {
+    const port = process.env.PORT || 8000;
+    app.listen(port);
+    console.log(`listening on ${port}....`);
+  })
+  .catch((err) => {
+    console.log(err);
+    process.exit(1);
+  });
--- util/log.js
+++ util/log.js
@@ -3,10 +3,10 @@ devErr is used for developers and contains those error which occured on the serv
 and should be seen by developers
 userErr :- beacause user did something wrong like invalid input etc.
 */
-const fs = require('fs');
-const util = require('util');
+const fs = require("fs");
+const util = require("util");
 let appendFile = util.promisify(fs.appendFile);
-const path = require('path');
+const path = require("path");
 /**
  *pass obj (has three properties) response: which was sent to the client,
  *isError:if there was any error, if true pass devErr:which is error
@@ -14,33 +14,32 @@ const path = require('path');
  * @param {*} obj
  */
 module.exports.log = async (obj) => {
-	const {
-		response,
-		isError,
-		devErr
-	} = obj;
-	try {
-		if (isError) {
-			console.log(devErr);
-			await this.writeErrLog(devErr);
-		}
-		await this.writeResponseLog(response);
-	} catch (error) {
-		console.error("logging service failed");
-		console.log("mail developers");
-	}
+  const { response, isError, devErr } = obj;
+  try {
+    if (isError) {
+      console.log(devErr);
+      await this.writeErrLog(devErr);
+    }
+    await this.writeResponseLog(response);
+  } catch (error) {
+    console.error("logging service failed");
+    console.log("mail developers");
+  }
 };
 
 exports.writeErrLog = async function writeErrLog(error) {
-	await appendFile(
-		path.join(process.cwd(), "logs", "error_logs.log"),
-		"\nerror on " + new Date().toString() + " error is " + JSON.stringify(error)
-	);
+  await appendFile(
+    path.join(process.cwd(), "logs", "error_logs.log"),
+    "\nerror on " + new Date().toString() + " error is " + JSON.stringify(error)
+  );
 };
 
 exports.writeResponseLog = async function writeResponseLog(responseObj) {
-
-	await appendFile(path.join(process.cwd(), "logs", "response_logs.log"),
-		"\n response sent on " + new Date().toString() + " is " + JSON.stringify(responseObj)
-	);
+  await appendFile(
+    path.join(process.cwd(), "logs", "response_logs.log"),
+    "\n response sent on " +
+      new Date().toString() +
+      " is " +
+      JSON.stringify(responseObj)
+  );
 };
