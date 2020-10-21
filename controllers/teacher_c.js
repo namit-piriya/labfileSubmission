2b987231b330f5060a3f9fe37b6c225fac941317
namit-piriya <namitpiriya945@gmail.com>
2020-10-21 20:29:36 +0530

    teacher_c.js:added controller addLabFiles basic checks need to have advanced user data checks

--- controllers/teacher_c.js
+++ controllers/teacher_c.js
@@ -1,5 +1,6 @@
 const TeacherModel = require("../models/teacher_m");
-const { isEmpty } = require("lodash");
+const { isEmpty, isArray } = require("lodash");
+const { checkSubcode } = require("../util/gen_util");
 const log = require("../util/log").log;
 const checkEmail = require("../util/gen_util").checkEmail;
 const checkPass = require("../util/gen_util").checkPass;
@@ -36,6 +37,31 @@ module.exports.registerTeacher = async (req, res) => {
   let result;
   try {
     result = await TeacherModel.registerTeacher(email, name, password, dept);
+
+    if (result === true) {
+      response = {
+        code: 200,
+        apiCode: "success",
+        msg: "Successfully registered,Please contact hod to get verified",
+      };
+    } else if (result === "EMAIL_EXISTS") {
+      response = {
+        code: 200,
+        apiCode: "EMAIL_EXISTS",
+        msg: "requested email already exists with another user",
+      };
+    } else {
+      response = {
+        code: 200,
+        apiCode: "student_email_exists",
+        msg: "same email in students account exist",
+      };
+    }
+
+    res.status(response.code).json(response);
+    return await log({
+      response: response,
+    });
   } catch (error) {
     isError = true;
     devErr = error;
@@ -51,41 +77,95 @@ module.exports.registerTeacher = async (req, res) => {
       response: response,
     });
   }
-
-  if (result === true) {
-    response = {
-      code: 200,
-      apiCode: "success",
-      msg: "Successfully registered,Please contact hod to get verified",
-    };
-  } else if (result === "EMAIL_EXISTS") {
-    response = {
-      code: 200,
-      apiCode: "EMAIL_EXISTS",
-      msg: "requested email already exists with another user",
-    };
-  } else {
-    response = {
-      code: 200,
-      apiCode: "student_email_exists",
-      msg: "same email in students account exist",
-    };
-  }
-
-  res.status(response.code).json(response);
-  return await log({
-    response: response,
-  });
 };
 
-// module.exports.addNoOfFiles = async (req, res) => {
+/**
+ *addLabfile of a given subject
+ * requires dept(from token) and files which is an array with name of the labfiles
+ * files:{fileNames:["name1","name2"],subcode:IT301}
+ * check whether the teacher is really assigned the subject sent in the request
+ * @param {*} req
+ * @param {*} res
+ */
+module.exports.addLabFiles = async (req, res) => {
+  let response, isError, devErr;
+  const { body } = req;
+  const { dept } = body.data;
+  const { files } = body;
+  console.log(files);
+  const fileNames = files.fileNames;
+  const subcode = files.subcode;
 
-// };
+  if (
+    !files ||
+    isEmpty(fileNames) ||
+    !subcode ||
+    !checkSubcode(subcode, dept)
+  ) {
+    response = {
+      code: 200,
+      msg: "please enter correct input!!",
+    };
+    res.status(response.code).json(response);
+    return await log({ response });
+  }
 
-// module.exports.unverifiedStudents = async (req, res) => {
+  try {
+    const dbResult = await TeacherModel.addLabFiles(dept, subcode, fileNames);
+    if (dbResult) {
+      response = {
+        msg: "labfiles added successfully",
+        code: 200,
+      };
+    } else {
+      response = {
+        msg: errMessage,
+        code: 500,
+      };
+    }
+  } catch (error) {
+    isError = true;
+    devErr = error;
+    response = {
+      msg: errMessage,
+      code: 500,
+    };
+  } finally {
+    res.status(response.code).json(response);
+    return await log({
+      response,
+      isError,
+      devErr,
+    });
+  }
+};
 
-// };
+module.exports.getUnverifiedStudents = async (req, res) => {};
 
-// module.exports.assignDueDate = async (req, res) => {
+module.exports.assignDueDateToLabFile = async (req, res) => {};
 
-// };
+module.exports.getSubjectsAssigned = async (req, res) => {
+  let { dept, email } = req.body.data;
+  let isError, response, devErr;
+  try {
+    let result = await TeacherModel.getSubjectsAssigned(dept, email);
+    if (!isArray(result)) {
+      response = {
+        error: true,
+        code: 500,
+        msg: errMessage,
+      };
+    } else response = { code: 200, teaches, error: false };
+  } catch (error) {
+    isError = true;
+    devErr = error;
+    response = {
+      error: true,
+      code: 500,
+      msg: errMessage,
+    };
+  } finally {
+    res.status(response.code).json(response);
+    return log({ response, isError, devErr });
+  }
+};
