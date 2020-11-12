4b16d10bd3b7c48a8e7432ff530300b445d76283
namit-piriya <namitpiriya945@gmail.com>
2020-11-12 18:44:09 +0530

    added route for get_assigned_subjects

--- routes/teacher_r.js
+++ routes/teacher_r.js
@@ -1,12 +1,17 @@
-const router = require('express').Router();
-const isAuthorized = require('../middleware/middleware').isAuthorized;
-const teacherController = require('../controllers/teacher_c');
+const router = require("express").Router();
+const isAuthorized = require("../middleware/middleware").isAuthorized;
+const teacherController = require("../controllers/teacher_c");
 
 router.post("/register_teacher", teacherController.registerTeacher);
 
 // router.post("/add_no_of_files", isAuthorized, teacherController.addNoOfFiles);
 
-// router.get("/get_unverified_students", isAuthorized, teacherController.unVerifiedStudents);
+// get email and dept from the token no need to send any data
+router.get(
+  "/get_assigned_subjects",
+  isAuthorized,
+  teacherController.getAssignedSubjects
+);
 
 // router.get("/assign_due_date", isAuthorized, teacherController.assignDueDate);
 
