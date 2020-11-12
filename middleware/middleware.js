401f3c5d8e80a7296f6e3b5b6c8c3489e0577f4d
namit-piriya <namitpiriya945@gmail.com>
2020-11-12 18:35:34 +0530

    minor changes removed some unused code added some comments

--- middleware/middleware.js
+++ middleware/middleware.js
@@ -1,64 +1,59 @@
 const lognsend = require("../util/log").log;
 const jwtUtil = require("../util/token_util");
 
+// check authorization
+/**
+ * @param {*} req
+ * @param {*} res
+ * @param {*} next
+ * @return {*} nothing
+ * checks if a user is loggedIn using jwt
+ * if not sends a reponse saying user not loggedIn
+ */
 module.exports.isAuthorized = async (req, res, next) => {
-	let response, isError, devErr;
-	const authHeader = req.headers.authorization;
-	if (authHeader) {
-		const token = authHeader.split(" ")[1];
-		try {
-			const data = await jwtUtil.verifyToken(token);
-			req.body.data = data;
-			next();
-			return;
-		} catch (error) {
-			// console.log(error.message + "in isAuthorized");
-			if (error.message === "invalid signature") {
-				response = {
-					userErr: "Please login again,token expired",
-					code: 403
-				}
-			} else {
-				isError = true;
-				devErr = error;
-				response = {
-					msg: errMessage,
-					code: 500
-				}
-			}
-			res.status(response.code).json(response);
-			return await lognsend({
-				response,
-				isError,
-				devErr
-			})
-		}
-	} else {
-		response = {
-			code: 403,
-			error: false,
-			userErr: "you are not logged in",
-		}
-		res.status(response.code).json(response);
-		return await lognsend({
-			response
-		});
-	}
+  let response, isError, devErr;
+  const authHeader = req.headers.authorization;
+  if (authHeader) {
+    const token = authHeader.split(" ")[1];
+    try {
+      const data = await jwtUtil.verifyToken(token);
+      req.body.data = data;
+      next();
+      return;
+    } catch (error) {
+      // console.log(error.message + "in isAuthorized");
+      if (
+        error.message === "invalid signature" ||
+        error.message.includes("jwt expired")
+      ) {
+        response = {
+          userErr: "Please login again,token expired",
+          code: 403,
+        };
+      } else {
+        isError = true;
+        devErr = error;
+        response = {
+          msg: errMessage,
+          code: 500,
+        };
+      }
+      res.status(response.code).json(response);
+      return await lognsend({
+        response,
+        isError,
+        devErr,
+      });
+    }
+  } else {
+    response = {
+      code: 403,
+      error: false,
+      userErr: "you are not logged in",
+    };
+    res.status(response.code).json(response);
+    return await lognsend({
+      response,
+    });
+  }
 };
-
-module.exports.midError = (req, res, next) => {
-	process.on("uncaughtException", (req, res) => {
-		console.log("inside midError");
-		return res.send("error");
-	});
-	next();
-};
-
-// to do how you let user login as hod
-
-// module.exports.configured = (req, res) => {
-//   if (!isConfigured) {
-//     return res.redirect("/auth/login_user");
-//   }
-//   next();
-// };
